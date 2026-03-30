c8bbe83a59f1a433a73029c8cda78d1aafa207a2
Kaua Santos <kauansantsdev@gmail.com>
2026-03-30 07:34:03 -0300

    feat: cria classe Conta com atributos básicos

---
+++ src/main/java/bank/Main.java
@@ -0,0 +1,14 @@
+package bank;
+
+import bank.model.Conta;
+
+public class Main {
+    public static void main(String[]args) {
+
+        Conta conta = new Conta();
+
+        System.out.println("Conta criada com sucesso!");
+
+    }
+
+}
---
+++ src/main/java/bank/model/Conta.java
@@ -0,0 +1,9 @@
+package bank.model;
+
+public class Conta {
+
+    private String numero;
+    private String titular;
+    private double saldo;
+
+}
